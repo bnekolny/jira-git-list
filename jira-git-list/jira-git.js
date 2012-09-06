0732a7e0243dae1fadeeee285f60068690b6d7b5
bnekolny <nekolny.brett@gmail.com>
2012-09-06 15:11:08 -0600

    Modified the jira git plugin to give suez merges

--- jira-git-list/jira-git.js
+++ jira-git-list/jira-git.js
@@ -1,4 +1,4 @@
-    var branches = [],
+    var branches = [], suez_branches = [],
         dashboards =    ["Filter Results: Ready for Staging QA",
                         "Filter Results: Ready for Code Review",
                         "Filter Results: Ready for Production Deployment"];
@@ -12,16 +12,32 @@
                 if(subsystem === "None" || subsystem.search('panama') > -1 ){
                     branches.push(row.attr('data-issuekey'));
                 }
+                else if(subsystem.search('suez') > -1){
+                    suez_branches.push(row.attr('data-issuekey'));
+                }
             });
         } // if
     }); // each
     
-    if( branches.length ){
-        var string = 'git merge ';
-        for(var i=0; i<branches.length; i++){
-            string += ' origin/' + branches[i];
+    if( branches.length || suez_branches.length){
+        if(branches.length){
+            var string = 'git merge ';
+            for(var i=0; i<branches.length; i++){
+                string += ' origin/' + branches[i];
+            }
         }
-        prompt('for the git', string);
+        if(suez_branches.length){
+            var suez_string = 'git merge ';
+            for(var i=0; i<suez_branches.length; i++){
+                suez_string += ' origin/' + suez_branches[i];
+            }
+        }
+        if(suez_branches.length){
+            alert("for panama\n" + string + "\n\nfor suez\n" + suez_string);
+        }
+        else{
+            prompt('for panama', string);
+        }
     }
     else {
         alert('No Branches');
